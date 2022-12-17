1ebf9cde1f38015f056755a78a9174134130ade0
Alexander <mypost@example.com>
2022-12-17 15:58:53 +0300

    проверка на четность

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача проверки четностт числа
+
+Console.WriteLine("Пожалуйста, введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit % 2 == 0){
+    Console.WriteLine($"Число {digit} - четное");
+}
+else {
+    Console.WriteLine($"Число {digit} - не четное");
+}
+
+
